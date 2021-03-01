35e2a7253a5d24b2ae50916ee7294033d6dad834
Tom Kerby <46465501+tjkerby@users.noreply.github.com>
2021-03-01 09:37:13 -0700

    Add files via upload

---
+++ scripts/fastasingleline.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+# Prints FASTA files with sequences in a single line each
+
+#Change into the alignment directory and run the script. NOTE: The opuput is written on the Screen, so aou have to parse it into a file using >
+#$ cd PATH_TO/aa_aligned
+#$ perl fastasingleline.pl FASTAFILE.fas > FASTAFILE_noninterleaved.fas
+#If you want to run more than one file at once, you have to run it using a loop, for example: 
+#$ for i in *.linsi.aa.fas; do perl fastasingleline.pl $i > $i.noninterleaved; done
+#OUTPUT: #The output files have the additional suffix .fas.noninterleaved (non-interleaved), e.g., EOG50003G.aa.linsi.fas.noninterleaved
+
+use strict;
+use warnings;
+
+die "Usage: $0 FASTAFILE\n" unless (scalar @ARGV);
+
+foreach (@ARGV) {
+	my $fh = Seqload::Fasta->open($_);
+	while (my ($h, $s) = $fh->next_seq()) {
+		printf(">%s\n%s\n", $h, $s);
+	}
+	undef $fh;
+}
+
+#
+# Module for easy reading of fasta files 
+#
+package Seqload::Fasta;
+use strict;
+use warnings;
+use Carp;
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw( fasta2csv check_if_fasta );
+
+# Constructor. Returns a sequence database object.
+sub open {
+  my ($class, $filename) = @_;
+  open (my $fh, '<', $filename)
+    or confess "Fatal: Could not open $filename\: $!\n";
+  my $self = {
+    'filename' => $filename,
+    'fh'       => $fh
+  };
+  bless($self, $class);
+  return $self;
+}
+
+# Returns the next sequence as an array (hdr, seq). 
+# Useful for looping through a seq database.
+sub next_seq {
+  my $self = shift;
+  my $fh = $self->{'fh'};
+	# this is the trick that makes this work
+  local $/ = "\n>"; # change the line separator
+  return unless defined(my $item = readline($fh));  # read the line(s)
+  chomp $item;
+  
+  if ($. == 1 and $item !~ /^>/) {  # first line is not a header
+    croak "Fatal: " . $self->{'filename'} . " is not a FASTA file: Missing descriptor line\n";
+  }
+
+	# remove the '>'
+  $item =~ s/^>//;
+
+	# split to a maximum of two items (header, sequence)
+  my ($hdr, $seq) = split(/\n/, $item, 2);
+	$hdr =~ s/\s+$//;	# remove all trailing whitespace
+  $seq =~ s/>//g if defined $seq;
+  $seq =~ s/\s+//g if defined $seq; # remove all whitespace, including newlines
+
+  return($hdr, $seq);
+}
+
+# Closes the file and undefs the database object.
+sub close {
+  my $self = shift;
+  my $fh = $self->{'fh'};
+  my $filename = $self->{'filename'};
+  close($fh) or carp("Warning: Could not close $filename\: $!\n");
+  undef($self);
+}
+
+# Destructor. This is called when you undef() an object
+sub DESTROY {
+  my $self = shift;
+  $self->close;
+}
+
